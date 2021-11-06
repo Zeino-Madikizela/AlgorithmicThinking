6524531466de8e3dc195acbb79fcf9094814e4c1
Zeino Madikizela <zeinosite@gmail.com>
2021-11-06 22:04:54 +0200

    Class LinearSearch:
    Declared and initialized the array

---
+++ src/searchAlgorithms/LinearSearch.java
@@ -0,0 +1,10 @@
+package searchAlgorithms;
+
+public class LinearSearch
+{
+    public static void main(String[] args)
+    {
+
+        int[] myArray = {5,3,5,4,9,4,6,4,65,61,6,54,654,13,21,35,46,1,};
+    }
+}
