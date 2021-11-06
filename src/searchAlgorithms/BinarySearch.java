498032b1b8c71fa52f6fffb93136e2709e535b59
Zeino Madikizela <zeinosite@gmail.com>
2021-11-06 22:01:29 +0200

    Class BinarySearch:
    Removed the :
    array initialization
    Scanner
    Prompter and output strings

--- src/searchAlgorithms/BinarySearch.java
+++ src/searchAlgorithms/BinarySearch.java
@@ -2,51 +2,37 @@ package searchAlgorithms;
 import java.util.Scanner;
 
 public class BinarySearch {
-    public static void main(String[] args) {
-        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 52, 58, 204, 351, 352, 353, 354, 3001, 3151, 3251, 3255};
-        Scanner input = new Scanner(System.in);
+    int array[];
+    int target;
 
-        System.out.print("Enter the target here : ");
-        int target = input.nextInt();
-
-        System.out.println("The target " + target + " is in index : " + binarySearch(myArray, target) );
+    public BinarySearch(int array[])
+    {
+        this.array = array;
     }
 
-    public static int binarySearch(int[] array, int target)
+    public void setTarget(int target) {
+        this.target = target;
+    }
+
+    public int binarySearch(int array[], int target)
     {
-        int posMin, posMax, middle;
-        posMin = 0;
-        posMax = array.length;
-        boolean found = false;
+        int min = 0;
+        int mid;
+        int max = array.length - 1;
 
-        while(!found)
+        while(min <= max)
         {
-            middle = (posMax + posMin)/2;
-            System.out.println(middle);
+            mid = (min + max)/2;
+            if (target == array[mid])
+                return mid;
 
-            if(middle == posMax)
-                found = true;
+            else if(target > array[mid])
+                min = mid + 1;
 
-            if(target == array[middle])
-            {
-                found = true;
-                return middle;
-            }
-
-            else if(target > array[middle])
-            {
-                posMin = middle;
-                middle = (posMin + posMax)/2;
-            }
-
-            else if(target < array[middle])
-            {
-                posMax = middle;
-                middle = (posMin + posMax)/2;
-            }
+            else
+                max = mid -1 ;
 
         }
-
         return -1;
     }
 
