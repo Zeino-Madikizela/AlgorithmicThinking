8d9cd7708cb2d5ee47fdb4c6e164dd044f14b162
Zeino Madikizela <zeinosite@gmail.com>
2021-11-07 19:58:49 +0200

    Class LinearSearch:
    Linear Search method added to the LinearSearch class

--- src/searchAlgorithms/LinearSearch.java
+++ src/searchAlgorithms/LinearSearch.java
@@ -2,9 +2,14 @@ package searchAlgorithms;
 
 public class LinearSearch
 {
-    public static void main(String[] args)
+    public int linearSearch(int[] array, int target)
     {
-
-        int[] myArray = {5,3,5,4,9,4,6,4,65,61,6,54,654,13,21,35,46,1,};
+        int arraySize = array.length;
+        for(int i = 0; i < arraySize; ++i)
+        {
+            if(array[i] == target)
+                return i;
+        }
+        return -1;
     }
 }
