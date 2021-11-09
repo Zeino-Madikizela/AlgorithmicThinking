285e86e3d843b9595dfd01f95bc96588abe16a97
Zeino Madikizela <zeinosite@gmail.com>
2021-11-09 23:47:03 +0200

    Class BinarySearchTester:
    Added a test for Odd number of elements.
    All passed

--- src/testers/BinarySearchTester.java
+++ src/testers/BinarySearchTester.java
@@ -43,6 +43,40 @@ public class BinarySearchTester
            result += "Test 4 \t:\t Failed - Target 4548 should be -1\n";
 
        System.out.println("Even number of elements\n" + result);
+
+       result = "";
+
+       int[] myArray2 = {1, 2, 3, 4, 5, 6, 3001, 3151, 3251};
+       myArray = myArray2;
+       /**
+        * Tests for an array with odd number of elements
+        */
+       //Target 1
+       if(binarySearch.binarySearch(myArray,1) == 0)
+           result += "Test 1 \t:\t Passed\n";
+
+       else if(binarySearch.binarySearch(myArray,1) != 0)
+           result += "Test 1 \t:\t Failed - Target 1 should be 0\n";
+
+       if(binarySearch.binarySearch(myArray,3001) == 6)
+           result += "Test 2 \t:\t Passed\n";
+
+       else if(binarySearch.binarySearch(myArray,3001) != 6)
+           result += "Test 2 \t:\t Failed - Target 3001 should be 6\n";
+
+       if(binarySearch.binarySearch(myArray, 3251) == 8)
+           result += "Test 3 \t:\t Passed\n";
+
+       else if(binarySearch.binarySearch(myArray, 3255) != 8)
+           result += "Test 3 \t:\t Failed - Target 3255 should be 8\n";
+
+       if(binarySearch.binarySearch(myArray, 4548) == -1)
+           result += "Test 4 \t:\t Passed";
+
+       else if(binarySearch.binarySearch(myArray, 4548) != -1)
+           result += "Test 4 \t:\t Failed - Target 4548 should be -1\n";
+
+       System.out.println("\nOdd number of elements\n" + result);
    }
 
    public static void tests(int[] array, String testTittle,String explanation ,int result)
