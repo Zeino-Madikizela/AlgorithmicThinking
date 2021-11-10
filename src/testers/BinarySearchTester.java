30460c0c6e111bbcc884b7f5fe6f793e94985309
Zeino Madikizela <zeinosite@gmail.com>
2021-11-10 23:15:57 +0200

    Class BinarySearchTester:
    Made a new reference array, targetArray - this will reference all cases being tested

--- src/testers/BinarySearchTester.java
+++ src/testers/BinarySearchTester.java
@@ -7,8 +7,9 @@ public class BinarySearchTester
 {
    public static void main(String[] args)
    {
-       int[] myArray = {1, 2, 3, 4, 5, 6, 3001, 3151, 3251, 3255};
-       BinarySearch binarySearch = new BinarySearch(myArray);
+       int[] targetArray;
+       int[] evenArray = {1, 2, 3, 4, 5, 6, 3001, 3151, 3251, 3255};
+       BinarySearch binarySearch = new BinarySearch(evenArray);
        Scanner input = new Scanner(System.in);
 
        String result = "";
@@ -18,65 +19,69 @@ public class BinarySearchTester
         * Tests for an array with even number of elements
         */
        //Target 1
-       if(binarySearch.binarySearch(myArray,1) == 0)
+       targetArray = evenArray;
+       if(binarySearch.binarySearch(targetArray,1) == 0)
            result += "Test 1 \t:\t Passed\n";
 
-       else if(binarySearch.binarySearch(myArray,1) != 0)
+       else if(binarySearch.binarySearch(targetArray,1) != 0)
            result += "Test 1 \t:\t Failed - Target 1 should be 0\n";
 
-       if(binarySearch.binarySearch(myArray,3001) == 6)
+       if(binarySearch.binarySearch(targetArray,3001) == 6)
            result += "Test 2 \t:\t Passed\n";
 
-       else if(binarySearch.binarySearch(myArray,3001) != 6)
+       else if(binarySearch.binarySearch(targetArray,3001) != 6)
            result += "Test 2 \t:\t Failed - Target 3001 should be 6\n";
 
-       if(binarySearch.binarySearch(myArray, 3255) == 9)
+       if(binarySearch.binarySearch(targetArray, 3255) == 9)
            result += "Test 3 \t:\t Passed\n";
 
-       else if(binarySearch.binarySearch(myArray, 3255) != 9)
+       else if(binarySearch.binarySearch(targetArray, 3255) != 9)
            result += "Test 3 \t:\t Failed - Target 3255 should be 9\n";
 
-       if(binarySearch.binarySearch(myArray, 4548) == -1)
+       if(binarySearch.binarySearch(targetArray, 4548) == -1)
            result += "Test 4 \t:\t Passed";
 
-       else if(binarySearch.binarySearch(myArray, 4548) != -1)
+       else if(binarySearch.binarySearch(targetArray, 4548) != -1)
            result += "Test 4 \t:\t Failed - Target 4548 should be -1\n";
 
        System.out.println("Even number of elements\n" + result);
 
        result = "";
 
-       int[] myArray2 = {1, 2, 3, 4, 5, 6, 3001, 3151, 3251};
-       myArray = myArray2;
+       int[] oddArray = {1, 2, 3, 4, 5, 6, 3001, 3151, 3251};
+       targetArray = oddArray;
        /**
         * Tests for an array with odd number of elements
         */
        //Target 1
-       if(binarySearch.binarySearch(myArray,1) == 0)
+       if(binarySearch.binarySearch(targetArray,1) == 0)
            result += "Test 1 \t:\t Passed\n";
 
-       else if(binarySearch.binarySearch(myArray,1) != 0)
+       else if(binarySearch.binarySearch(targetArray,1) != 0)
            result += "Test 1 \t:\t Failed - Target 1 should be 0\n";
 
-       if(binarySearch.binarySearch(myArray,3001) == 6)
+       if(binarySearch.binarySearch(targetArray,3001) == 6)
            result += "Test 2 \t:\t Passed\n";
 
-       else if(binarySearch.binarySearch(myArray,3001) != 6)
+       else if(binarySearch.binarySearch(targetArray,3001) != 6)
            result += "Test 2 \t:\t Failed - Target 3001 should be 6\n";
 
-       if(binarySearch.binarySearch(myArray, 3251) == 8)
+       if(binarySearch.binarySearch(targetArray, 3251) == 8)
            result += "Test 3 \t:\t Passed\n";
 
-       else if(binarySearch.binarySearch(myArray, 3255) != 8)
+       else if(binarySearch.binarySearch(targetArray, 3255) != 8)
            result += "Test 3 \t:\t Failed - Target 3255 should be 8\n";
 
-       if(binarySearch.binarySearch(myArray, 4548) == -1)
+       if(binarySearch.binarySearch(targetArray, 4548) == -1)
            result += "Test 4 \t:\t Passed";
 
-       else if(binarySearch.binarySearch(myArray, 4548) != -1)
+       else if(binarySearch.binarySearch(targetArray, 4548) != -1)
            result += "Test 4 \t:\t Failed - Target 4548 should be -1\n";
 
        System.out.println("\nOdd number of elements\n" + result);
+
+       System.out.println("\nTests for an array of 1 element");
+
    }
 
    public static void tests(int[] array, String testTittle,String explanation ,int result)
